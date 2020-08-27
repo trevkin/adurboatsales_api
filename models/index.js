9bdafc5c29dc12a9d19a7fd486a9d9e1c10c900b
trevk <trev@candidsoftware.com>
2020-08-27 15:22:27 +0100

    really fixed port this time

--- models/index.js
+++ models/index.js
@@ -21,6 +21,7 @@ const sequelize = new Sequelize(
     config.PASSWORD,
     {
         host: config.HOST,
+        port: config.PORT,
         dialect: config.dialect,
         operatorsAliases: false,
 
